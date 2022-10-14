4b2563ad6fa0490ca9a8691a70b5c692182c3bf7
Symplice BONI <bonisimplice@gmail.com>
2022-10-14 22:28:47 +0000

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,10 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char alphabet;	
+	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		if (alphabet != 'q' && alphabet != 'e')
 			putchar(alphabet);
