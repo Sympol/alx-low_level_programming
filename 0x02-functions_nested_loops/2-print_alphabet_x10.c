581d48d45e7a5ad975867647b62fc3ea9a4eac6a
Symplice BONI <bonisimplice@gmail.com>
2022-10-19 05:20:07 +0000

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
-  * print_alphabet_x10 - Print alphabet x10 times
+  * print_alphabet_x10 - generate alphabet x10 times
+  * Description : print the alphabet x10 times
   *
   * Return: void
   */
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0;
-
-	while (i <= 9)
+	int x = 0;
+	while (x < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-
+		x++;
 		_putchar('\n');
-
-		i++;
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,5 @@
 
 char _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10()
+void print_alphabet_x10();
 #endif /* MAIN_H*/
