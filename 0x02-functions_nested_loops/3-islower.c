3ca16f60f1a85c360951b720e6439a9c7c186fca
Symplice BONI <bonisimplice@gmail.com>
2022-10-19 05:59:35 +0000

    sign

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 /**
   * _islower - checks character.
   * description : checks for lowercase character.
-  *
+  * @c: single letter input
   * Return: 1 or 0
   */
 int _islower(int c)
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 /**
   * _isalpha - checks for alphabetic character.
   * description: a function that checks for alphabetic character 
+  * @c: single letter input
   * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
   */
 int _isalpha(int c)
