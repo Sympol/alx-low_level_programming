07e29ebdc9f9db7a1be90cb5429213abfc2ac343
Symplice BONI <bonisimplice@gmail.com>
2022-10-21 05:17:27 +0000

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "mainn.h"
+
+/**
+ * print_most_numbers - print the numbers, from 0 to 9, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		if (c != '2' && c != '4')
+		{
+		_putchar (c);
+		}
+	}
+	_putchar ('\n');
+}
