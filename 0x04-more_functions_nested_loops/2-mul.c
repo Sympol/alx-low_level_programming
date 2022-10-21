25719ac3206bff4e493c4c0759c258afae275710
Symplice BONI <bonisimplice@gmail.com>
2022-10-21 05:02:37 +0000

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplie two integers
+ *@a: int var
+ *@b: int var
+ * Return: result of axb
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
