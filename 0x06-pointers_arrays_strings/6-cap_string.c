efbdad7dce910e68310835b0cb89e48c8c9ea549
Symplice BONI <bonisimplice@gmail.com>
2022-10-27 23:18:27 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of a string.
+ *@a: pointer
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *a)
+{
+	int i;
+	int j;
+	char k[] = " \t\n,;.!\?\"(){}";
+
+	for (i = 0 ; a[i] != '\0'; i++)
+	{
+		for (j = 0 ; k[j] != '\0'; j++)
+		{
+			if (a[i] == k[j])
+			{
+				if (a[i + 1] > 96 && a[i + 1] < 123)
+				{
+					a[i + 1] = a[i + 1] - 32;
+				}
+			}
+			else if (i == 0)
+			{
+				if (a[i] > 96 && a[i + 1] < 123)
+				{
+					a[i] = a[i] - 32;
+				}
+			}
+		}
+	}
+	return (a);
+}
