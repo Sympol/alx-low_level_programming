adbd138713c35103caae2aaed8c6bda2117134c1
Symplice BONI <bonisimplice@gmail.com>
2022-11-01 02:44:49 +0000

    crackme

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+* set_string - set the value of a pointer to a char
+* @s:  pointer to the adderss of string s
+* @to: string to set value to
+*
+* Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
