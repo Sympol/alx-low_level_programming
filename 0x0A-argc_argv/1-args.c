985d63a73c9299dc137686be89deff27074820bf
Symplice BONI <bonisimplice@gmail.com>
2022-11-08 12:06:22 +0000

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the number of arguments
+ * @argc: argument count
+ * @argv: Argument vector for values
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	argc--;
+	printf("%d\n", argc);
+	return (0);
+}
